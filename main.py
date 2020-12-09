aac94c5252cf6819b9e2fade7db7793fced3013d
gr-jones <gtj4@pitt.edu>
2020-12-09 12:56:32 -0500

    added method to plot and save performance data

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+import pandas as pd  
 
 import random
 import torch
@@ -116,6 +120,11 @@ def train(model, criterion, optimizer, loader, useCuda=False):
 
     print('\t\tTrain: \tAcc %5.2f  Loss %.3f' % (
         100 * total_correct / total_samples, total_loss / total_samples))
+    
+    train_accuracy = total_correct / total_samples
+    train_loss = total_loss / total_samples
+
+    return train_accuracy, train_loss
 
 
 def test(model, loader, useCuda=False):
@@ -137,6 +146,10 @@ def test(model, loader, useCuda=False):
             total_samples += len(target)
 
         print('\t\t Test: \tAcc %5.2f' % (100 * total_correct / total_samples))
+
+    test_accuracy = total_correct / total_samples
+
+    return test_accuracy
 
 
 ###############################################################################
@@ -208,23 +221,61 @@ def test(model, loader, useCuda=False):
 #               total_samples))
 ###############################################################################
 
+def plot_and_save_performance(train_accuracy, train_loss, test_accuracy, algo, num_epochs):
+    
+    # plot network performance
+    x = range(num_epochs)
+
+    plt.plot(x, train_accuracy, '-', label='Training Accuracy')
+    plt.plot(x, test_accuracy, '-', label='Test Accuracy')
+    plt.legend(loc='best')
+
+    plt.xlabel("Epoch")
+    plt.ylabel("Classification Accuracy %")
+    plt.title("Network Performance Using Approximate Gradient")
+
+    seperator = "."
+    plt.savefig(seperator.join([algo,'png']))
+    plt.show()
+    
+    # save train and test data to csv file
+        
+    # dictionary of lists   
+    dict = {'train_accuracy': train_accuracy, 'train_loss': train_loss, 'test_accuracy': test_accuracy}   
+        
+    df = pd.DataFrame(dict)  
+
+    filetype = 'csv'
+    filename = [algo, filetype]
+    seperator = '.'
+
+    # saving the dataframe  
+    df.to_csv(seperator.join(filename)) 
+
+
+
 def main(batch_size=128, num_epochs=30, useCuda=True):
 
-    # model = SNN(
-    #     input_dim=784,
-    #     output_dim=10,
-    #     T=20,
-    #     dt=1,
-    #     tau_m=20.0,
-    #     tau_s=5.0,
-    #     mu=0.1,
-    #     backprop=SNN.EVENTPROP).cuda()
+    # Uncomment if using EventProp algo
+    algo = 'EventProp'
+    model = SNN(
+        input_dim=784,
+        output_dim=10,
+        T=20,
+        dt=1,
+        tau_m=20.0,
+        tau_s=5.0,
+        mu=0.1,
+        backprop=SNN.EVENTPROP).cuda()
+    
 
-    model = SNU(
-        input_size=784,
-        hidden_size=10,
-        decay=1)
-
+    # Uncomment if using SpikeProp/gradient approximate algo
+    # algo = 'GradApprox'
+    # model = SNU(
+    #     input_size=784,
+    #     hidden_size=10,
+    #     decay=1)
+    
     if useCuda:
         model = model.cuda()
 
@@ -239,11 +290,23 @@ def main(batch_size=128, num_epochs=30, useCuda=True):
 
     train_loader, test_loader = loadData(batch_size)
 
+    train_accuracy = []
+    train_loss = []
+    test_accuracy = []
     for epoch in range(1, num_epochs+1):
         print('Epoch %d/%d' % (epoch, num_epochs))
-        train(model, criterion, optimizer, train_loader, useCuda)
-        test(model, test_loader, useCuda)
+        train_acc, loss = train(model, criterion, optimizer, train_loader, useCuda)
+        test_acc = test(model, test_loader, useCuda)
+
+        train_accuracy.append(train_acc)
+        train_loss.append(loss)
+        test_accuracy.append(test_acc)
+
         scheduler.step()
+    
+    plot_and_save_performance(train_accuracy, train_loss, test_accuracy, algo, num_epochs)
+
+ 
 
 if __name__ == '__main__':
     main()
