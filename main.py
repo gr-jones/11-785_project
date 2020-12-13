ac34e0c2150f40fd4776261e88dffdc36e833723
Henry Du <henrytong98@gmail.com>
2020-12-13 17:15:51 -0500

    fixed bug in main

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-import matplotlib
-import matplotlib.pyplot as plt
+import sys
 
-import pandas as pd  
-
-import random
 import torch
 
-import numpy as np
-
 from torch import optim
-
 from torch import nn
 
-import torch.nn.functional as F
 from torchvision import datasets, transforms
 
 from snn.model import SNN, SNU
 from snn.loss import SpikeCELoss
+from util import plot_and_save_performance
 
 
-def loadData(batch_size):
+def load_data(batch_size):
     train_dataset = datasets.MNIST(
         'data',
         train=True,
@@ -45,15 +38,11 @@ def loadData(batch_size):
     return train_loader, test_loader
 
 
-def encode_data(data, t_min=2.0, t_max=12.0, T=20):
+def encode_data(data, t_min=2, t_max=12, T=20):
     spike_data = t_min + (t_max - t_min) * (1 - data).view(data.shape[0], -1)
-    spike_data = F.one_hot(spike_data.long(), int(T)).float()
+    spike_data = nn.functional.one_hot(spike_data.long(), int(T)).float()
     return spike_data
 
-# def encode_data_eventprop(data, t_min=2.0, t_max=12.0, T=20):
-#     spike_data = t_min + (t_max - t_min) * (data < 0.5).view(data.shape[0], -1)
-#     spike_data = F.one_hot(spike_data.long(), int(T))
-#     return spike_data
 
 # def encode_data_repeat(data, T=20):
 #     spike_data = (data.view(data.shape[0], -1) > 0.5).float()
@@ -68,15 +57,17 @@ def encode_data(data, t_min=2.0, t_max=12.0, T=20):
 #     return spikes
 
 
-def print_grads(named_parameters):
+def print_grads(named_parameters, fliter0=False):
     for n, p in named_parameters:
-            if p.requires_grad:
-                avg_grad = p.grad.abs().mean()
-                if avg_grad:
-                    print('parameter [%s]: %f' % (n, avg_grad))
+        if p.requires_grad:
+            avg_grad = p.grad.abs().mean()
+            if fliter0 and not avg_grad:
+                continue
+            print('parameter [%s]: %f' % (n, avg_grad))
 
 
-def train(model, criterion, optimizer, loader, useCuda=False):
+def train(model, criterion, optimizer, loader, alpha=0.01, beta=2.0,
+          useCuda=False):
     total_correct = 0
     total_loss = 0
     total_samples = 0
@@ -96,20 +87,31 @@ def train(model, criterion, optimizer, loader, useCuda=False):
         # if batch_idx % 100 == 0:
         #     print(output[0,:])
 
-        loss = criterion(-output, target)
+        loss = criterion(output, target)
 
-        predictions = output.detach().argmax(dim=1)
+        
+
+        if type(model) == SNU:
+            predictions = output.detach().argmax(dim=1)
+        else:
+            if alpha != 0:
+                target_first_spike_times = output.gather(1, target.view(-1, 1))
+                loss += alpha * (torch.exp(target_first_spike_times / (
+                    beta * 5)) - 1).mean()
+            predictions = output.detach().argmin(dim=1)
         # if batch_idx % 100 == 0:
         #     print(predictions[0:10])
         #     print(target[0:10])
 
         batch_samples = len(target)
         batch_correct = (predictions == target).sum().item()
-        batch_loss = loss.item() * batch_samples
-        
+        batch_loss = loss.item()
+        if type(model) == SNU:
+            batch_loss = 1 / batch_loss
+
         total_samples += batch_samples
         total_correct += batch_correct
-        total_loss += batch_loss
+        total_loss += batch_loss * batch_samples
 
         loss.backward()
 
@@ -120,177 +122,75 @@ def train(model, criterion, optimizer, loader, useCuda=False):
         if batch_idx % 100 == 0:
             print('\tBatch %3d/%d: \tAcc %5.2f  Loss %.3f' % (
                 batch_idx, num_batches, 100 * batch_correct / batch_samples,
-                batch_loss / batch_samples))
+                batch_loss))
 
-    print('\t\tTrain: \tAcc %5.2f  Loss %.3f' % (
-        100 * total_correct / total_samples, total_loss / total_samples))
-    
-    train_accuracy = total_correct / total_samples
+    train_acc = 100 * total_correct / total_samples
     train_loss = total_loss / total_samples
 
-    return train_accuracy*100, 1/train_loss
+    print('\t' + '-' * 37)
+    print('\t\tTrain: \tAcc %5.2f  Loss %.3f' % (train_acc, train_loss))
+
+    return train_acc, train_loss
 
 
-def test(model, loader, useCuda=False):
+def test(model, criterion, loader, useCuda=False):
     total_correct = 0
+    total_loss = 0
     total_samples = 0
     model.eval()
 
-    with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(loader):
-            if useCuda:
-                data, target = data.cuda(), target.cuda()
+    for batch_idx, (data, target) in enumerate(loader):
+        if useCuda:
+            data, target = data.cuda(), target.cuda()
 
-            spike_data = encode_data(data)
+        data = encode_data(data)
 
-            first_post_spikes = model(spike_data)
-            predictions = first_post_spikes.data.max(1, keepdim=True)[1]
-            total_correct += predictions.eq(
-                target.data.view_as(predictions)).sum().item()
-            total_samples += len(target)
+        output = model(data)
 
-        print('\t\t Test: \tAcc %5.2f' % (100 * total_correct / total_samples))
+        loss = criterion(output, target)
 
-    test_accuracy = total_correct / total_samples
+        if type(model) == SNU:
+            predictions = output.detach().argmax(dim=1)
+            loss = 1 / loss
+        else:
+            predictions = output.detach().argmin(dim=1)
 
-    return test_accuracy*100
+        total_correct += (predictions == target).sum().item()
+        total_loss += loss.item() * len(target)
+        total_samples += len(target)
 
+    test_acc = 100 * total_correct / total_samples
+    test_loss = total_loss / total_samples
 
-###############################################################################
-# Old training and testing functions for event prop below:
+    print('\t\t Test: \tAcc %5.2f  Loss %.3f' % (test_acc, test_loss))
 
-# def train(model, criterion, optimizer, loader, alpha=0.01, beta=2.0):
-#     total_correct = 0
-#     total_loss = 0
-#     total_samples = 0
-#     model.train()
-
-#     num_batches = len(loader)
-
-#     for batch_idx, (x, target) in enumerate(loader):
-#         x, target = x.cuda(), target.cuda()
-#         x = encode_data(x)
-
-#         output = model(x)
-
-#         loss = criterion(output, target)
-
-#         if alpha != 0:
-#             target_first_spike_times = output.gather(1, target.view(-1, 1))
-#             loss += alpha * (torch.exp(target_first_spike_times / (
-#                 beta * model.tau_s)) - 1).mean()
-
-#         predictions = output.data.min(1, keepdim=True)[1]
-
-#         batch_correct = predictions.eq(
-#             target.data.view_as(predictions)).sum().item()
-#         batch_loss = loss.item() * len(target)
-#         batch_samples = len(target)
-
-#         total_correct += batch_correct
-#         total_loss += batch_loss
-#         total_samples += batch_samples
-
-#         optimizer.zero_grad()
-#         loss.backward()
-
-#         optimizer.step()
-
-#         if batch_idx % 100 == 0:
-#             print('\tBatch %3d/%d: \tAcc %4.2f  Loss %.3f' % (
-#                 batch_idx, num_batches, 100 * batch_correct / batch_samples,
-#                 batch_loss / batch_samples))
-
-#     print('\t\tTrain: \tAcc %.2f  Loss %.3f' % (
-#         100 * total_correct / total_samples, total_loss / total_samples))
+    return test_acc, test_loss
 
 
-# def test(model, loader):
-#     total_correct = 0
-#     total_samples = 0
-#     model.eval()
+def main(batch_size=128, num_epochs=50, exactGrad=True, useCuda=True):
+    if exactGrad:
+        print('#' * 45 + '\nRunning Eventprop:\n' + '-' * 45)
+        model = SNN(
+            input_dim=784,
+            output_dim=10,
+            T=20,
+            dt=1,
+            tau_m=20.0,
+            tau_s=5.0,
+            mu=0.1)
+        criterion = SpikeCELoss(xi=0.4, tau_s=5)
+    else:
+        print('#' * 45 + '\nRunning SNUP:\n' + '-' * 45)
+        model = SNU(
+            input_size=784,
+            output_size=10,
+            decay=.95)
+        criterion = SpikeCELoss(xi=0.2, tau_s=5)
 
-#     with torch.no_grad():
-#         for batch_idx, (data, target) in enumerate(loader):
-#             data, target = data.cuda(), target.cuda()
-#             spike_data = encode_data(data)
-
-#             first_post_spikes = model(spike_data)
-#             predictions = first_post_spikes.data.min(1, keepdim=True)[1]
-#             total_correct += predictions.eq(
-#                 target.data.view_as(predictions)).sum().item()
-#             total_samples += len(target)
-
-#         print('\t\t Test: \tAcc %4.2f' % (100 * total_correct / 
-#               total_samples))
-###############################################################################
-
-def plot_and_save_performance(train_accuracy, train_loss, test_accuracy, algo, num_epochs):
-    
-    # plot network performance and save plot
-    x = range(num_epochs)
-
-    plt.plot(x, train_accuracy, '-', label='Training Accuracy')
-    plt.plot(x, test_accuracy, '-', label='Test Accuracy')
-    plt.legend(loc='best')
-
-    plt.xlabel("Epoch")
-    plt.ylabel("Classification Accuracy %")
-    plt.title("Network Performance Using Approximate Gradient")
-    
-    plt.savefig(".".join([algo,'png'])) # save network performance plot
-    plt.show()
-    
-    # plot training loss and save plot 
-    plt.plot(x, train_loss, '-', label='Training Loss')
-    plt.legend(loc='best')
-
-    plt.xlabel("Epoch")
-    plt.ylabel("loss")
-    plt.title("Network Training Loss Using Approximate Gradient")
-
-    filename = "".join([algo,'_loss'])
-    plt.savefig(".".join([filename,'png'])) # save training loss plot
-    plt.show()
-    
-    # save train and test data to csv file
-    dict = {'train_accuracy': train_accuracy,
-            'train_loss': train_loss, 
-            'test_accuracy': test_accuracy}   
-        
-    df = pd.DataFrame(dict)  
-
-    # saving the dataframe  
-    df.to_csv(".".join([algo,'csv'])) 
-
-
-
-def main(batch_size=128, num_epochs=50, useCuda=True):
-
-    # Uncomment if using EventProp algo
-    # algo = 'EventProp'
-    # model = SNN(
-    #     input_dim=784,
-    #     output_dim=10,
-    #     T=20,
-    #     dt=1,
-    #     tau_m=20.0,
-    #     tau_s=5.0,
-    #     mu=0.1,
-    #     backprop=SNN.EVENTPROP).cuda()
-    
-
-    # Uncomment if using SpikeProp/gradient approximate algo
-    algo = 'GradApprox'
-    model = SNU(
-        input_size=784,
-        hidden_size=10,
-        decay=1)
-    
     if useCuda:
         model = model.cuda()
 
-    criterion = nn.CrossEntropyLoss()
+    
 
     optimizer = torch.optim.SGD(model.parameters(), lr=1)
 
@@ -299,25 +199,27 @@ def main(batch_size=128, num_epochs=50, useCuda=True):
         step_size=10,
         gamma=0.1)
 
-    train_loader, test_loader = loadData(batch_size)
+    train_loader, test_loader = load_data(batch_size)
 
-    train_accuracy = []
-    train_loss = []
-    test_accuracy = []
+    train_performance = []
+    test_performance = []
     for epoch in range(1, num_epochs+1):
         print('Epoch %d/%d' % (epoch, num_epochs))
-        train_acc, loss = train(model, criterion, optimizer, train_loader, useCuda)
-        test_acc = test(model, test_loader, useCuda)
+        train_performance.append(train(
+            model, criterion, optimizer, train_loader, useCuda=useCuda))
 
-        train_accuracy.append(train_acc)
-        train_loss.append(loss)
-        test_accuracy.append(test_acc)
-
+        with torch.no_grad():
+            test_performance.append(test(
+                model, criterion, test_loader, useCuda=useCuda))
         scheduler.step()
-    
-    plot_and_save_performance(train_accuracy, train_loss, test_accuracy, algo, num_epochs)
 
- 
+    return train_performance, test_performance
+
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        apprx_performance = main(exactGrad=False)
+        exact_performance = main(exactGrad=True)
+        plot_and_save_performance(exact_performance, apprx_performance)
+    else:
+        main(exactGrad=False)
